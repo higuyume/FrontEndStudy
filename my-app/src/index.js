4a6f8c25793040c4741849826c5a71c853a8fd1e
higuyume <higuyumekd@gmail.com>
2019-04-17 09:07:00 +0900

    チュートリアル一通り完成

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -47,12 +47,13 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{squares: Array(9).fill(null),}],
+      stepNumber: 0,
       xIsNext:true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice(); 
 
@@ -62,13 +63,21 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext?'X':'0';
     this.setState({
       history : history.concat([{squares:squares,}]),
+      stepNumber:history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber:step,
+      xIsNext:(step % 2) ===0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     
     const moves = history.map((step,move)=>{
@@ -76,7 +85,7 @@ class Game extends React.Component {
       'Go to move #' + move:
       'Go to game start';
       return(
-        <li>
+        <li key={move}>
           <button onClick={()=>this.jumpTo(move)}>{desc}</button>
         </li>
       )
